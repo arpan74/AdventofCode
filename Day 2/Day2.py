a82dfe5fe3ed5c4f330182a7ab7116bfe4069f8b
arpan74 <arpan74@gmail.com>
2016-05-14 22:16:04 -0400

    Delete Day2.py

--- Day 2/Day2.py
+++
@@ -1,9 +0,0 @@
-def surfaceArea(l, w, h):
-	return 2*(l * w) + 2*(w * h) + 2*(l * h) + min((l*w),(w*h),(l*h))
-sum = 0
-
-for line in open("input.txt"):
-	strA = line.split("x")
-	sum = sum + surfaceArea(int(strA[0]), int(strA[1]), int(strA[2]))
-print sum
-
