981879af254c765071412d0daddf24c493e697ec
Arpan Gupta <arpan74@gmail.com>
2016-05-18 20:15:41 -0400

    Coordinate system for Day 3

---
+++ Day 3/Day3.py
@@ -0,0 +1,19 @@
+usrinput = open("input.txt").read()
+
+x = []
+
+
+xCor = 0
+yCor = 0
+for i in usrinput:
+	
+	if( i == "^"):
+		yCor = yCor + 1
+	if( i == "v"):
+		yCor = yCor - 1
+	if( i == ">"):
+		xCor = xCor + 1
+	if( i == "<"):
+		xCor = xCor - 1
+		
+	if( xCor in x):
