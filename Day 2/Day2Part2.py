14116977594f09583f74162869146d9e1fb2c77a
Arpan Gupta <arpan74@gmail.com>
2016-05-20 00:24:46 -0400

    Added part 2 of day 2

---
+++ Day 2/Day2Part2.py
@@ -0,0 +1,14 @@
+def DetermineRib(l, w, h):
+	surfA = l*w*h
+	if( min((l*w),(w*h),(l*h) == l*w ):
+		return (l + l + w + w) + surfA 
+	if( min((l*w),(w*h),(l*h) == w*h ):
+		return (w + w + h + h) + surfA
+	return (l + l + h + h ) + surfA
+sum = 0
+
+for line in open("input.txt"):
+	strA = line.split("x")
+	sum = sum + DetermineRib(int(strA[0]), int(strA[1]), int(strA[2]))
+print sum
+
